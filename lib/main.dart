9e5073f9ee08a835fbe8f64c03f0df4754ac8406
PhRezende-eng <ph.hr.001@gmail.com>
2021-05-23 16:15:54 -0300

    Intializing the course

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:app/screens/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+}
