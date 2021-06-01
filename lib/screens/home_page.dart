fba074f75c577c3f3a94f243938996528aedee31
PhRezende-eng <ph.hr.001@gmail.com>
2021-05-31 22:33:25 -0300

    learning about textformfield

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -9,6 +9,7 @@ class _HomePageState extends State<HomePage> {
   var perguntaRespondida = 0;
 
   void responder() {
+    // if perguntaRespondida = perguntas.
     setState(() {
       //ser ver para notifcar a interface gráfica que houve uma mudança, pois ela está alterando a variável
       perguntaRespondida++;
@@ -42,14 +43,26 @@ class _HomePageState extends State<HomePage> {
             onPressed: responder,
           ),
           Container(
-            margin: EdgeInsets.only(left: 20, right: 50),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+              ),
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            padding: EdgeInsets.only(left: 20, right: 60),
+            margin: EdgeInsets.only(left: 10, right: 10),
             child: TextFormField(
               cursorHeight: 20,
               cursorColor: Colors.black,
               decoration: InputDecoration(
-                icon: Icon(Icons.ac_unit_outlined),
+                icon: Icon(
+                  Icons.ac_unit_outlined,
+                ),
                 hintText: "hintext",
                 labelText: "labeltext",
+                border: InputBorder.none,
               ),
               validator: (String? value) {
                 return (value != null && value.contains('@'))
