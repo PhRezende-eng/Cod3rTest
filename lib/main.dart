d392aac7d10cfd46284ebf92241d386729cfb2b1
PhRezende-eng <ph.hr.001@gmail.com>
2021-06-09 23:53:36 -0300

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         appBar: AppBar(
           title: Text('COD3R'),
           centerTitle: true,
